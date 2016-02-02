a6fcb4f90cc5da009c914bf66314d9f40259a637
LupenTiJay <jayant.tiwari777@gmail.com>
2016-02-02 16:46:16 -0500

    First commit for Namecheap

---
+++ Artifical Algorithm.java
@@ -0,0 +1,36 @@
+//Algorithm 
+
+void setup(){
+	
+	size(500,500);
+
+}
+
+int [] center = nre [] int{ width / 2, height / 2};
+ArrayList <
+
+
+
+
+void CheckAdjacent(){
+
+
+}
+
+
+public class Square(){
+Square top;
+Square left;
+Squre bottom;
+Square right;
+
+	public Square(int x, int y, int shade){
+		fill(shade);
+		rect(1,1,x,y);
+	}
+
+	
+
+
+}
+
